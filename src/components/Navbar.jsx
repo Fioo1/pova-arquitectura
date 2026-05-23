b98d53795f4ec7aab9fee2b56f3b86c423f0ee52
Fioo1 <171174808+Fioo1@users.noreply.github.com>
2026-05-22 21:48:01 -0500

    agregando pestañas a NOSOTROS

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,64 @@
+import logo from "../assets/logo.jpg"
+
 export default function Navbar() {
   return (
     <nav className="fixed top-0 left-0 w-full bg-white/90 backdrop-blur-md shadow-sm z-50">
       <div className="max-w-7xl mx-auto flex justify-between items-center px-6 py-4">
-        <h1 className="text-2xl font-bold tracking-wide">
-          POVA Arquitectura
-        </h1>
+        <div className="flex items-center gap-3">
+			<img
+				src={logo}
+				alt="POVA Logo"
+				className="w-12 h-12 object-contain"
+			/>
+			
+			<h1 className="text-2xl font-bold tracking-wide">
+				POVA Arquitectura
+			</h1>
+		</div>
 
         <div className="hidden md:flex gap-8 text-sm font-medium">
           <a href="/#inicio" className="hover:text-gray-500 transition">
             Inicio
           </a>
+		  
+		<div className="relative group">
+			<button className="hover:text-gray-500 transition">
+				Nosotros
+			</button>
 
-          <a href="/#nosotros" className="hover:text-gray-500 transition">
-            Nosotros
-          </a>
+			<div
+				className="
+					absolute
+					left-0
+					top-full
+					pt-2
+					opacity-0
+					invisible
+					group-hover:opacity-100
+					group-hover:visible
+					transition-all
+					duration-300
+					z-50
+				"
+  			>
+
+				<div className="bg-white shadow-xl rounded-2xl p-6 w-64">
+					<a
+						href="/#nosotros"
+						className="block py-3 hover:text-gray-500 transition"
+					>
+						¿Quiénes somos?
+					</a>
+
+					<a
+						href="/#servicios"
+						className="block py-3 hover:text-gray-500 transition"
+					>
+						Nuestro proceso
+					</a>
+				</div>
+			</div>
+		</div>
 
           <a href="/#proyectos" className="hover:text-gray-500 transition">
             Proyectos
